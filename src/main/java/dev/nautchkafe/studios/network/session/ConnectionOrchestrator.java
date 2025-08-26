7a50f371272e290edde2575777bbb56d62829b2e
Damian <damian.festowski@onet.pl>
2025-08-26 21:51:32 +0200

    (?): fix bug directory

--- src/main/java/dev/nautchkafe/studios/network/session/ConnectionOrchestrator.java
+++
@@ -1,8 +0,0 @@
-package dev.nautchkafe.studios.network.session;
-
-import dev.nautchkafe.studios.network.session.contract.ConnectionContract;
-
-interface ConnectionOrchestrator {
-
-    SessionChoice<String, ConnectionContract> orchestratePlayerConnection(final String playerId, String targetInstance);
-}
